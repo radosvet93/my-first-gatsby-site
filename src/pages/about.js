d9f16f15f4f2d14452aeb33a910445bef598b4c0
Radosvet Petrov <93radosvet@gmail.com>
2021-08-21 11:53:10 +0100

    Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Page">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
